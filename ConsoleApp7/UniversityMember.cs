85f8fcea64a5f119b4f4c1e3adcd5ad0fdd2781c
bruro <roman.brus.25@kse.org.ua>
2026-03-05 19:16:36 +0200

    Added class UniversityMember

---
+++ ConsoleApp7/UniversityMember.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp7;
+
+public class UniversityMember
+{
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set => _name = string.IsNullOrWhiteSpace(value) ? 
+            throw new Exception("Invalid name") : value;
+    }
+    
+    public string MemberId { get; private set; }
+
+    protected List<string> ActionLog = new List<string>();
+
+    public UniversityMember(string name, string memberId)
+    {
+        Name = name;
+        MemberId = memberId;
+    }
+
+    public virtual void PerformDuties()
+    {
+        if (this.ActionLog.Count == 5)
+        {
+            throw new Exception("The member has already reached a daily limit of 5 actions");
+        }
+    }
+}
